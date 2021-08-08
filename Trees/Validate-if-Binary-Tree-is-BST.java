97c9615cf6db5ba386392747668973bfe268a5b5
shahnupur1901 <67814929+shahnupur1901@users.noreply.github.com>
2021-08-08 17:31:20 +0530

    Update Validate-if-Binary-Tree-is-BST.java

--- Trees/Validate-if-Binary-Tree-is-BST.java
+++ Trees/Validate-if-Binary-Tree-is-BST.java
@@ -2,8 +2,30 @@
 Left Subtree elements should be < root
 Right Subtree elements should be > root
 All other node's subtrees must be BSTs
-*/
 
+
+Approach 1 :
+1. Maintain a minimum and maximum range of values
+2. If the node's value is not within this range return false
+3. Call the check function again for left and right subtrees. Update the max for the left call, and min for the right call.
+*/
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return check(root,null,null);
+    }
+    boolean check(TreeNode lroot, Integer min, Integer max){
+        if(lroot==null){
+            return true;
+        }
+        else if((min!=null && lroot.val<=min )||(max!=null && lroot.val >= max)){//if duplicates not allowed, allow = both sides
+            return false;
+        }
+        else if(!check(lroot.left,min,lroot.val) || !check(lroot.right,lroot.val,max))
+            return false;
+        else
+            return true;
+    }
+}
 class Solution {
     public boolean isValidBST(TreeNode root) {
         if(root==null || (root.left==null && root.right==null))//leaf node is also a BST
