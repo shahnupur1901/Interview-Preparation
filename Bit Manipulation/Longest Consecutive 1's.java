e98d44415d7ab84c9a3f5dc872488a0ac632485b
shahnupur1901 <67814929+shahnupur1901@users.noreply.github.com>
2021-08-11 22:04:37 +0530

    Create Longest Consecutive 1's.java

---
+++ Bit Manipulation/Longest Consecutive 1's.java
@@ -0,0 +1,24 @@
+class Solution{
+    
+    /*  Function to calculate the longest consecutive ones
+    *   N: given input to calculate the longest consecutive ones
+    */
+    public static int maxConsecutiveOnes(int n) {
+        
+        // Your code here\
+        int start = 0, max = 0, current = 0;
+        int numDigits = (int)(Math.log(n) / Math.log(2)) + 1;
+        int res = n;
+        while(res!=0){
+            int digit = res & 1;
+            if(digit==1)
+                current++;
+            if(max < current)
+                max = current;
+            if(digit==0)
+                current = 0;
+            res = res >> 1;
+        }
+        return max;
+    }
+}
